7ded756109068970c06904430709c92ea5090fd6
yuluji <yuji.hirano@malme.co.jp>
2024-12-22 16:35:55 +0900

    using Autodesk.Aec.DatabaseServices;を削除

--- PropertySetViewer.cs
+++ PropertySetViewer.cs
@@ -2,7 +2,6 @@ using Autodesk.AutoCAD.ApplicationServices;
 using Autodesk.AutoCAD.DatabaseServices;
 using Autodesk.AutoCAD.EditorInput;
 using Autodesk.AutoCAD.Runtime;
-using Autodesk.Aec.DatabaseServices;
 using System;
 using System.Collections.Generic;
 using System.Linq;
